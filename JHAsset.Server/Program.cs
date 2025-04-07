a7c9180b454f8f78bb328249a677e42f3cd258a2
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-04-07 18:58:43 +0900

    프로젝트 누락 초기 설정 개선 3

--- JHAsset.Server/Program.cs
+++ JHAsset.Server/Program.cs
@@ -1,11 +1,15 @@
 using JHAsset.Shared.Data;
 using JHAsset.Shared.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.FluentUI.AspNetCore.Components;
+using JHAsset;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Blazor Server
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
 builder.Services.AddServerSideBlazor();
 
 // EF Core (SQLite)
@@ -31,5 +35,13 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
